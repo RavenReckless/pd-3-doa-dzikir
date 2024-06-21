1efb42f435d801dc5f7115e7418d145eb9a954af
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-06-21 11:14:06 +0700

    update: adding UI for form profile edit for admin role

--- resources/views/admin/adminprofile.blade.php
+++ resources/views/admin/adminprofile.blade.php
@@ -39,33 +39,6 @@
                   </div>
                   <form action="#">
                     <div class="p-6.5">
-                      <div class="mb-4.5 flex flex-col gap-6 xl:flex-row">
-                        <div class="w-full xl:w-1/2">
-                          <label
-                            class="mb-3 block text-sm font-medium text-black dark:text-white"
-                          >
-                            First name
-                          </label>
-                          <input
-                            type="text"
-                            placeholder="Enter your first name"
-                            class="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                          />
-                        </div>
-
-                        <div class="w-full xl:w-1/2">
-                          <label
-                            class="mb-3 block text-sm font-medium text-black dark:text-white"
-                          >
-                            Last name
-                          </label>
-                          <input
-                            type="text"
-                            placeholder="Enter your last name"
-                            class="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                          />
-                        </div>
-                      </div>
 
                       <div class="mb-4.5">
                         <label
@@ -164,7 +137,7 @@
 
               <div class="flex flex-col gap-9">
                 <!-- Sign In Form -->
-                <div
+                {{-- <div
                   class="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark"
                 >
                   <div
@@ -250,7 +223,7 @@
                       </button>
                     </div>
                   </form>
-                </div>
+                </div> --}}
 
                 <!-- Sign Up Form -->
                 <div
