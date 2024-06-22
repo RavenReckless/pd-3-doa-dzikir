3f69a375c9b51ed44f841a62042ed5b7c6b0c1f1
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-06-22 11:01:10 +0700

    fix: fixing all html templates to blade file

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -47,8 +47,7 @@
           <li>
             <a
               class="sidebar-menu @yield('menuDashboard') group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
-              href="#"
-              @click.prevent="selected = (selected === 'Dashboard' ? '':'Dashboard')"
+              href="{{url('/admin')}}"
               :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Dashboard') || (page === 'ecommerce' || page === 'analytics' || page === 'stocks') }"
             >
               <svg
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,7 +7,7 @@
         <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
             <div class="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
                 <h2 class="text-title-md2 font-bold text-black dark:text-white">
-                    Tables
+                    Users Table
                 </h2>
 
                 <nav>
